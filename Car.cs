db00dd4a2e4da4c195bbe849dc7ec1a7f4d20bd4
JigerSingh <124743224+JigerSinghh@users.noreply.github.com>
2023-05-23 23:04:51 -0400

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Application
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(string make, string model, int year)
+            {
+            Make = make;
+            Model = model;
+            Year = year;
+
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
+
